8bc1bc368541227d3a8bcd278c9c767d2c07e1df
Arise <2778368047@qq.com>
2024-08-16 18:55:03 +0800

    pref: 更新dao模板文件，并重新生成dao文件

--- sms_model/sms_dao/internal/sms_service_provider_config.go
+++ sms_model/sms_dao/internal/sms_service_provider_config.go
@@ -16,9 +16,13 @@ import (
 
 // SmsServiceProviderConfigDao is the data access object for table sms_service_provider_config.
 type SmsServiceProviderConfigDao struct {
-	table   string                          // table is the underlying table name of the DAO.
-	group   string                          // group is the database configuration group name of current DAO.
-	columns SmsServiceProviderConfigColumns // columns contains all the column names of Table for convenient usage.
+	dao_interface.IDao
+	table       string                          // table is the underlying table name of the DAO.
+	group       string                          // group is the database configuration group name of current DAO.
+	columns     SmsServiceProviderConfigColumns // columns contains all the column names of Table for convenient usage.
+	daoConfig   *dao_interface.DaoConfig
+	ignoreCache bool
+	exWhereArr  []string
 }
 
 // SmsServiceProviderConfigColumns defines and stores column names for table sms_service_provider_config.
@@ -66,10 +70,15 @@ func NewSmsServiceProviderConfigDao(proxy ...dao_interface.IDao) *SmsServiceProv
 	var dao *SmsServiceProviderConfigDao
 	if len(proxy) > 0 {
 		dao = &SmsServiceProviderConfigDao{
-			group:   proxy[0].Group(),
-			table:   proxy[0].Table(),
-			columns: smsServiceProviderConfigColumns,
+			group:       proxy[0].Group(),
+			table:       proxy[0].Table(),
+			columns:     smsServiceProviderConfigColumns,
+			daoConfig:   proxy[0].DaoConfig(context.Background()),
+			IDao:        proxy[0].DaoConfig(context.Background()).Dao,
+			ignoreCache: proxy[0].DaoConfig(context.Background()).IsIgnoreCache(),
+			exWhereArr:  proxy[0].DaoConfig(context.Background()).Dao.GetExtWhereKeys(),
 		}
+
 		return dao
 	}
 
@@ -105,28 +114,25 @@ func (dao *SmsServiceProviderConfigDao) Ctx(ctx context.Context, cacheOption ...
 	return dao.DaoConfig(ctx, cacheOption...).Model
 }
 
-func (dao *SmsServiceProviderConfigDao) DaoConfig(ctx context.Context, cacheOption ...*gdb.CacheOption) dao_interface.DaoConfig {
-	daoConfig := dao_interface.DaoConfig{
-		Dao:   dao,
-		DB:    dao.DB(),
-		Table: dao.table,
-		Group: dao.group,
-		Model: dao.DB().Model(dao.Table()).Safe().Ctx(ctx),
+func (dao *SmsServiceProviderConfigDao) DaoConfig(ctx context.Context, cacheOption ...*gdb.CacheOption) *dao_interface.DaoConfig {
+	//if dao.daoConfig != nil && len(dao.exWhereArr) == 0 {
+	//	return dao.daoConfig
+	//}
+
+	var daoConfig = daoctl.NewDaoConfig(ctx, dao, cacheOption...)
+	dao.daoConfig = &daoConfig
+
+	if len(dao.exWhereArr) > 0 {
+		daoConfig.IgnoreExtModel(dao.exWhereArr...)
+		dao.exWhereArr = []string{}
+
 	}
 
-	if len(cacheOption) == 0 {
-		daoConfig.CacheOption = daoctl.MakeDaoCache(dao.Table())
-		daoConfig.Model = daoConfig.Model.Cache(*daoConfig.CacheOption)
-	} else {
-		if cacheOption[0] != nil {
-			daoConfig.CacheOption = cacheOption[0]
-			daoConfig.Model = daoConfig.Model.Cache(*daoConfig.CacheOption)
-		}
+	if dao.ignoreCache {
+		daoConfig.IgnoreCache()
 	}
 
-	daoConfig.Model = daoctl.RegisterDaoHook(daoConfig.Model)
-
-	return daoConfig
+	return dao.daoConfig
 }
 
 // Transaction wraps the transaction logic using function f.
@@ -137,4 +143,21 @@ func (dao *SmsServiceProviderConfigDao) DaoConfig(ctx context.Context, cacheOpti
 // as it is automatically handled by this function.
 func (dao *SmsServiceProviderConfigDao) Transaction(ctx context.Context, f func(ctx context.Context, tx gdb.TX) error) (err error) {
 	return dao.Ctx(ctx).Transaction(ctx, f)
+}
+
+func (dao *SmsServiceProviderConfigDao) GetExtWhereKeys() []string {
+	return dao.exWhereArr
+}
+
+func (dao *SmsServiceProviderConfigDao) IsIgnoreCache() bool {
+	return dao.ignoreCache
+}
+
+func (dao *SmsServiceProviderConfigDao) IgnoreCache() dao_interface.IDao {
+	dao.ignoreCache = true
+	return dao
+}
+func (dao *SmsServiceProviderConfigDao) IgnoreExtModel(whereKey ...string) dao_interface.IDao {
+	dao.exWhereArr = append(dao.exWhereArr, whereKey...)
+	return dao
 }
